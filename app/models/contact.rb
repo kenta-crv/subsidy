836563113a82e2607648cee0e8112cd534cad399
kenta-crv <adp@crold-valieat.jp>
2020-02-04 15:51:04 +0900

    2020_02_04

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,11 +2,11 @@ class Contact
   include ActiveModel::Model
   attr_accessor :company, :name, :tel, :email, :address, :subject ,:message
 
-  validates :company, {presence: true}
-  validates :name, {presence: true}
-  validates :tel, {presence: true}
-  validates :email, {presence: true}
-  validates :address, {presence: true}
+  validates :company, {presence: true} #会社名
+  validates :name, {presence: true} #担当者名
+  validates :tel, {presence: true} #電話番号
+  validates :email, {presence: true} #メールアドレス
+  validates :address, {presence: true} #
   validates :subject, {presence: true}
   validates :message, {presence: true}
 end
